4fcf87e9991bb6e69303bad6d3993465b228424f
Systool <fr4nc16@hotmail.it>
2017-03-16 23:26:41 +0100

    Add files via upload

---
+++ sketch_led_intermittenza_.ino
@@ -0,0 +1,26 @@
+void setup() {
+  //  fa lampeggiare il led per un po quando premi il pulsante
+pinMode(13,OUTPUT);
+pinMode(12,INPUT_PULLUP); 
+digitalWrite(13,LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int statopuls = digitalRead(12);
+if (statopuls==LOW)
+{ 
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(13,HIGH);
+    delay(200);
+  
+    digitalWrite(13,LOW);
+    delay(200);
+  }
+ }
+digitalWrite(13,LOW);
+}
+
+
