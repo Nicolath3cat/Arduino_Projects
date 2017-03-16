1ccdc915dd2b79dad79d25d7dd2e764900dc9c6e
Systool <fr4nc16@hotmail.it>
2017-03-16 23:27:21 +0100

    Add files via upload

---
+++ sketch_led_pulsante_v3.0.ino
@@ -0,0 +1,15 @@
+int statoled = 0;  
+void setup() { 
+  // Ti fa da interruttore on off col pulsante semza usare interruttore.
+pinMode(13,OUTPUT);
+pinMode(12,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ if(digitalRead(12)==LOW){
+  statoled =!statoled;  
+  digitalWrite(13,statoled);  
+  delay(250);
+ }
+}
