6e6d41a56c1c4038843ce177f9679db842962a73
Systool <fr4nc16@hotmail.it>
2017-03-16 23:27:42 +0100

    Add files via upload

---
+++ sketch_servo.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+#include <LiquidCrystal.h>
+
+
+Servo gianni;
+int pot=A3;
+int val;
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+gianni.attach(9);
+lcd.begin(16,2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val=analogRead(pot);
+val=map(val, 0, 1023, 0, 179);
+gianni.write(val);
+lcd.print(val);
+delay(15);
+}
